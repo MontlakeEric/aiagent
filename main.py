d2ac7671a7c294403da234b7d16754f15b2241c0
Eric <e@wedoitathome.com>
2026-07-25 12:22:06 -0700

    first call

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from aiagent!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+load_dotenv()  # Load environment variables from .env file
+api_key = os.getenv("OPENROUTER_API_KEY")
+if api_key is None:
+    raise RuntimeError("API key not found in environment variables")
 
-if __name__ == "__main__":
-    main()
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+messages = [
+    {
+        "role": "user",
+        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    }
+]
+
+result = client.chat.completions.create(
+    model="openrouter/free",
+    messages=messages,
+)
+
+print(result.choices[0].message.content)
