79133024297685cd698133d687d6dc353c6fa5e4
Eric <e@wedoitathome.com>
2026-07-27 13:34:16 -0700

    do actual file info collection and update tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,10 +5,17 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         working_directory_full = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_directory_full, directory))
         valid_target_dir = os.path.commonpath([working_directory_full, target_dir]) == working_directory_full
+
         if not valid_target_dir:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
-        return f'Success: "{directory}" is within the working directory'
+
+        files = os.listdir(target_dir)
+        result = []
+        for file in files:
+            file_path = os.path.join(target_dir, file)
+            result.append(f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
+        return "\n".join(result)
     except Exception as e:
         return f'Error: {str(e)}'
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,6 +1,19 @@
 from functions.get_files_info import get_files_info
 
-print(get_files_info("calculator", "."))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
-print(get_files_info("calculator", "main.py"))
+def print_result(result, friendly_name):
+    print(f"Result for {friendly_name} directory:")
+    if result[0] == "-":
+        print(result.replace("-", "  -"))
+    else:
+        print("    " + result)
+
+tests = [
+    ["calculator", ".", "current"],
+    ["calculator", "pkg", "'pkg'"],
+    ["calculator", "/bin", "'/bin'"],
+    ["calculator", "../", "'../'"],
+]
+
+for test in tests:
+    print_result(get_files_info(test[0], test[1]), test[2])
+
